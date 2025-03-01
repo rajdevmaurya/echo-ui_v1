67e742da96e9b4f3a1950505a00aa6c71c98c989
rajdevmaurya <rajdevmaurya@gmail.com>
2025-03-01 14:03:09 +0530

    Updated cards
    Updated cards

--- src/components/shared/JobCard.js
+++ src/components/shared/JobCard.js
@@ -7,46 +7,45 @@ import classes from './JobCard.module.css';
 
 function JobCard({ job, jobUrl, hideDescription, isCollection = false, showDialog, isFeatured = false }) {
   const cssClasses = `${classes['job-card']} ${isCollection ? classes['collection-card'] : ''} ${isFeatured ? classes['featured-card'] : ''}`;
+
+  // Conditional wrapper to make the entire card clickable
+  const CardWrapper = ({ children }) => (
+    jobUrl ? <Link to={`${jobUrl}${job.id}`} className={cssClasses} style={{ textDecoration: 'none', color: 'inherit' }}>{children}</Link> 
+           : <div className={cssClasses}>{children}</div>
+  );
+
   return (
-    <div className={cssClasses}>
+    <CardWrapper>
       <div className={`${classes['job-card-header']} common-img-header`}>
         <div className={`${classes['job-card-img']}`}>
-          {jobUrl ?
-            <Link to={`${jobUrl}${job.id}`}><Logo logoUrl={job.logoUrl} /></Link>
-            :
-            <Logo logoUrl={job.logoUrl} />
-          }
+          <Logo logoUrl={job.logoUrl} />
         </div>
         <div className={`${classes['job-card-content']}`}>
           <div>
+          <div className={`${classes['card-id']}`}  style={{ fontWeight: 'bold' }}>{job.title}</div>
             <div className={`${classes['card-identifier']}`}>
-              {jobUrl ?
-                <h2><Link to={`${jobUrl}${job.id}`} className='black-text'>{job.company}</Link></h2>
-                :
-                <h2 className='black-text'>{job.company}</h2>
-              }
+              <h2 className='black-text'>{job.brand}</h2>
             </div>
-            {!isCollection && <div className={`${classes['card-time']}`}>
+            {/*!isCollection && <div className={`${classes['card-time']}`}>
               <TimesAgo createDate={job.createDate} />
             </div>}
-            <div className={`${classes['card-id']}`}>{job.id}</div>
+            <div className={`${classes['card-id']}`}>{job.id}</div> */}
           </div>
-          {isCollection &&
-            <Button className="red lighten-4" varient='icon' onClick={() => showDialog(job)}>
+          {isCollection && (
+            <Button className="red lighten-4" varient='icon' onClick={(e) => { 
+              e.stopPropagation(); // Prevent navigation when clicking delete
+              showDialog(job);
+            }}>
               <i className="material-icons" style={{ color: 'var(--red)' }}>delete</i>
-            </Button>}
+            </Button>
+          )}
         </div>
       </div>
       {!isCollection && <div className='divider' style={{ margin: 0 }} />}
       <div className={`${classes['job-card-body']}`}>
-        {jobUrl ?
-          <h3 className="truncate flow-text"><Link className='black-text' to={`${jobUrl}${job.id}`}>{job.title}</Link></h3>
-          :
-          <h3 className="truncate flow-text black-text">{job.title}</h3>
-        }
-        {!hideDescription && <div className={`truncate ${classes['card-description']}`}>{job.description}</div>}
+      <div className={`truncate ${classes['card-description']}`}>{job.description}</div>
       </div>
-    </div>
+    </CardWrapper>
   );
 }
 
