d6559bfbfeb198162f92af0e02b3e1597e11daf9
rajdevmaurya <rajdevmaurya@gmail.com>
2025-03-28 19:21:19 +0530

    Updated

--- src/Constants.js
+++ src/Constants.js
@@ -9,5 +9,4 @@ const dev = {
     API_BASE_URL: process.env.REACT_APP_BASE_URL || 'https://echo-api-b2etapgqfwb3a5ae.centralindia-01.azurewebsites.net'
   }
 };
-
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
