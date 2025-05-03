e604b5f8062dcc0267fccb30383ac91b063e7434
rajdevmaurya <rajdevmaurya@gmail.com>
2025-05-03 16:53:36 +0530

    Updated Host Name
    Updated Host Name

--- src/Constants.js
+++ src/Constants.js
@@ -1,12 +1,12 @@
 const prod = {
   url: {
-    API_BASE_URL: 'https://148.66.156.153:8080',
+    API_BASE_URL: 'https://echohealthcare.in:8443',
   }
 };
 
 const dev = {
   url: {
-    API_BASE_URL: process.env.REACT_APP_BASE_URL || 'https://148.66.156.153:8080',
+    API_BASE_URL: process.env.REACT_APP_BASE_URL || 'https://echohealthcare.in:8443',
   }
 };
 export const config = process.env.NODE_ENV === 'development' ? dev : prod;
